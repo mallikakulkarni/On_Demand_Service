f132abd7a0508fee1edff58d89ca634f6c0ff211
Nagkumar <me@nagkumar.com>
2015-11-25 15:47:08 -0800

    Business Analytics data is ready

--- analytics/README.md
+++ analytics/README.md
@@ -10,13 +10,13 @@ Step 1: Installation
 --------------------
 Run the following commands when you are in the current directory.
 You can create virtualenv if you want (suggested). More information on creating the virtualenv [here](http://docs.python-guide.org/en/latest/dev/virtualenvs/)
-<p>`pip install -r requirements.txt`</p>
+``pip install -r requirements.txt``
 This will install all the necessary libraries to run the server.
 
 Step 2: Run Server
 ------------------
 Run this line on your terminal
-<p>`python manage.py runserver`</p>
+``python manage.py runserver``
 
 
 Using the API
@@ -25,26 +25,49 @@ Using the API
 Admin data
 ----------
 ###Total Number of users on the site till date can be obtained by the URL:
-<p>`/admin/total_visitors`</p>
+``/admin/total_visitors``
 >We are counting the number of unique ip_addresses and showing the result.
 
 ###Total Number of registered users on the site till date:
-<p>`/admin/total_registered_visitors`</p>
+``/admin/total_registered_visitors``
 >We are counting the number of unique user_id and showing the result.
 
 ###Visits per IP
-<p>`/admin/visits_by_users`</p>
+``/admin/visits_by_users``
 >We are counting the number of visits by ip_address and showing the result.
 
 ###Visits per user_id
-<p>`/admin/visits_by_registered_users`</p>
+``/admin/visits_by_registered_users``
 >We are counting the number of visits by unique user_id and showing the result.
 
 ###Daily visits
-<p>`/admin/daily_visits`</p>
+``/admin/daily_visits``
 >We are counting the number of users per day.
 
 ###Daily Registered Visits
-<p>`/admin/daily_registered_visits`</p>
+``/admin/daily_registered_visits``
 >We are counting the number of registered users visiting the site per day.
 
+Business data
+-------------
+In addition to the simple site metrics, we can fetch the following data for the admin.
+
+###Total Users who visit any of the business' page
+``/business/total_users_for_business?business_id=<business_id>``
+example usage:
+``/business/total_users_for_business?business_id=1``
+
+###Total Registered Users who visit any of the business' page
+``/business/total_registered_users_for_business?business_id=<business_id>``
+Example Usage:
+``/business/total_registered_users_for_business?business_id=1``
+
+###Total Registered Users who are checking your workers
+``/business/total_registered_users_checking_your_workers?business_id=<business_id>&worker_id=<w1>,<w2>``
+Example Usage
+``/business/total_registered_users_checking_your_workers?business_id=1&worker_id=1,2``
+
+###Daily visitors for any page of the business
+``/business/daily_registered_visits?business_id=<business_id>``
+Example Usage
+``/business/daily_registered_visits?business_id=1``
--- analytics/app.py
+++ analytics/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, jsonify, make_response
+from functools import wraps
+
+from flask import Flask, render_template, jsonify, make_response, request
 from pymongo import MongoClient
 from bson import json_util
 import json
@@ -7,6 +9,32 @@ app = Flask(__name__)
 client = MongoClient()
 db = client.log_database
 collection = db.log
+
+
+def add_response_headers(headers={}):
+    """This decorator adds the headers passed in to the response"""
+
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            resp = make_response(f(*args, **kwargs))
+            h = resp.headers
+            for header, value in headers.items():
+                h[header] = value
+            return resp
+
+        return decorated_function
+
+    return decorator
+
+
+def send_json(f):
+    @wraps(f)
+    @add_response_headers({'Content-Type': 'application/json'})
+    def decorated_function(*args, **kwargs):
+        return f(*args, **kwargs)
+
+    return decorated_function
 
 
 @app.route("/db_as_table")
@@ -22,6 +50,7 @@ def db_as_table():
 
 
 @app.route("/admin/total_visitors")
+@send_json
 def total_visitors():
     res = collection.distinct("ip_address")
     res = len(res)
@@ -29,6 +58,7 @@ def total_visitors():
 
 
 @app.route("/admin/total_registered_visitors")
+@send_json
 def total_registered_visitors():
     res = collection.distinct("user_id", {"user_id": {"$nin": ["", None]}})
     res = len(res)
@@ -36,6 +66,7 @@ def total_registered_visitors():
 
 
 @app.route("/admin/visits_by_users")
+@send_json
 def count_visits():
     res = list(db.log.aggregate([
         {"$group": {"_id": "$ip_address", "count": {"$sum": 1}}}
@@ -44,6 +75,7 @@ def count_visits():
 
 
 @app.route("/admin/visits_by_registered_users")
+@send_json
 def count_registered_visits():
     res = list(db.log.aggregate([
         {"$group": {"_id": "$user_id", "count": {"$sum": 1}}}
@@ -52,6 +84,7 @@ def count_registered_visits():
 
 
 @app.route("/admin/daily_visits")
+@send_json
 def daily_visits():
     res = list(db.log.aggregate([
         {
@@ -72,6 +105,7 @@ def daily_visits():
 
 
 @app.route("/admin/daily_registered_visits")
+@send_json
 def daily_registered_visits():
     res = list(db.log.aggregate([
         {
@@ -98,6 +132,102 @@ def daily_registered_visits():
         }
     ]))
     return make_response(json.dumps(res, default=json_util.default))
+
+
+@app.route("/business/total_users_for_business")
+def total_users_for_business():
+    business_id = request.args.get("business_id")
+    if business_id:
+        count = db.log.find({"business_id": int(business_id)}).count()
+        resp = make_response(json.dumps({"count": count}))
+        resp.headers['Content-Type'] = 'application/json'
+        return resp
+    else:
+        return render_template("error.html"), 400
+
+
+@app.route("/business/total_registered_users_for_business")
+def total_registered_users_for_business():
+    business_id = request.args.get("business_id")
+    if business_id:
+        count = db.log.find(
+            {
+                "business_id": int(business_id),
+                "user_id": {
+                    "$nin": ["", None]
+                }
+            }).count()
+        resp = make_response(json.dumps({"count": count}))
+        resp.headers['Content-Type'] = 'application/json'
+        return resp
+    else:
+        return render_template("error.html"), 400
+
+
+@app.route("/business/total_registered_users_checking_your_workers")
+def total_registered_users_checking_your_workers():
+    business_id = int(request.args.get("business_id"))
+    workers = request.args.get("workers")
+    workers = workers.split(",")
+    workers_for_query = []
+    for w in workers:
+        try:
+            workers_for_query.append(int(w))
+        except ValueError:
+            pass
+    if business_id and len(workers) > 0:
+        count = db.log.find(
+            {
+                "business_id": business_id,
+                "user_id": {
+                    "$nin": ["", None]
+                },
+                "worker_id": {
+                    "$in": workers_for_query
+                }
+            }).count()
+        print count
+        resp = make_response(json.dumps({"count": count}))
+        resp.headers['Content-Type'] = 'application/json'
+        return resp
+    else:
+        return render_template("error.html"), 400
+
+
+@app.route("/business/daily_registered_visits")
+def business_daily_registered_visits():
+    business_id = int(request.args.get("business_id"))
+    if business_id:
+        res = list(db.log.aggregate([
+            {
+                "$match":
+                    {
+                        "user_id":
+                            {
+                                "$gt": 0
+                            },
+                        "business_id": business_id
+                    }
+            },
+            {
+                "$group":
+                    {
+                        "_id":
+                            {
+                                "date": "$log_date"
+                            },
+                        "count":
+                            {
+                                "$sum": 1
+                            }
+                    }
+            }
+        ]))
+        resp = make_response(json.dumps({"results": res}, default=json_util.default))
+        resp.headers['Content-Type'] = 'application/json'
+        return resp
+    else:
+        return render_template("error.html"), 400
 
 
 if __name__ == "__main__":
