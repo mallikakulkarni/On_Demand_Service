2b9b375d66503ef925725a47c974734f3849794f
Nagkumar <me@nagkumar.com>
2015-11-30 21:45:20 -0800

    Added log as a service and a function in javascript front end

--- On_Demand_Service/app.js
+++ On_Demand_Service/app.js
@@ -29,6 +29,11 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 app.use('/', routes);
 app.use('/users', users);
 app.use('/business', business);
